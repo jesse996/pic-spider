dc20de1598b7d2d1d921e0b070f34c0137d80c29
jesse <szx961102@gmail.com>
2021-08-16 15:40:56 +0800

    add jd articles

--- jdlingyu.ts
+++ jdlingyu.ts
@@ -3,7 +3,7 @@ import type { Page } from 'puppeteer-core'
 import * as puppeteer from 'puppeteer-core'
 import { promises } from 'fs'
 const { readFile, writeFile } = promises
-
+import * as dayjs from 'dayjs'
 ;(async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -27,6 +27,21 @@ const { readFile, writeFile } = promises
     else interceptedRequest.continue() //弹出
   })
 
+  //图片
+  // await goPic(page)
+
+  //文章
+  await goArticles(page)
+
+  await browser.close()
+})()
+
+async function goto(page: Page, url: string) {
+  await Promise.all([page.waitForNavigation(), page.goto(url)])
+}
+
+async function goPic(page: Page) {
+  //图片
   await goto(page, 'https://www.jdlingyu.com/96018.html')
 
   while (true) {
@@ -53,19 +68,72 @@ const { readFile, writeFile } = promises
     await page.waitForTimeout(300)
     try {
       //下一页
-      await Promise.all([
-        page.waitForNavigation(),
-        page.click('.post-pre h2 a'),
-      ])
-      await page.waitForTimeout(100)
+      await clickNextPage(page, '.post-pre h2 a')
     } catch (e) {
       console.log(e)
       break
     }
   }
-  await browser.close()
-})()
+}
 
-async function goto(page: Page, url: string) {
-  await Promise.all([page.waitForNavigation(), page.goto(url)])
+async function goArticles(page: Page) {
+  await goto(page, 'https://www.jdlingyu.com/95707.html')
+
+  let lastCreateTime = null
+  let count = 0
+  while (true) {
+    await page.waitForSelector('.entry-header')
+    let title = await page.$eval('.entry-header h1', (el) => el.textContent)
+    let category = await page.$eval(
+      '.entry-header .post-list-cat a',
+      (el) => el.textContent
+    )
+    let content = await page.$eval('.entry-content', (el) => el.innerHTML)
+    let tags = await page.$$eval('.post-tags-meat .tag-text', (els) =>
+      els.map((el) => el.textContent)
+    )
+    let coverImg = await page.$eval('.entry-content img:first-of-type', (el) =>
+      el.getAttribute('src')
+    )
+
+    let postData = {
+      title,
+      category,
+      content,
+      tags,
+      coverImg,
+      createTime: lastCreateTime,
+    }
+    console.log(postData.title, lastCreateTime)
+
+    try {
+      await post('news', postData)
+    } catch (err) {
+      console.log(err)
+    }
+
+    //更新时间
+    lastCreateTime = await page.$eval(
+      '.post-pre-next-in p',
+      (el) => el.textContent
+    )
+    lastCreateTime = dayjs(lastCreateTime).toISOString()
+
+    // if (++count > 1) {
+    //   break
+    // }
+
+    try {
+      //下一页
+      await clickNextPage(page, '.post-pre h2 a')
+    } catch (e) {
+      console.log(e)
+      break
+    }
+  }
+}
+
+async function clickNextPage(page: Page, selector: string) {
+  await Promise.all([page.waitForNavigation(), page.click(selector)])
+  await page.waitForTimeout(100)
 }
